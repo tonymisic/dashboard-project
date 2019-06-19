a5268dbef37b7fb6b4f4dfde14730b81f366dbf9
Tony Misic <tony.misic98@gmail.com>
2019-06-19 12:14:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ class Application(tk.Frame):
         self.create_graph(canvas2, -0.61, 0.1, 900, 400, "-2 Far Too Dry", "+2 Far Too Humid")
 
         self.big_brain_graph(canvas1, [45,25,85,100,90,60,30,50], 50, 50)
+        self.menu_big_brain_graph(canvas1)
 
     def create_graph(self, canvas, median, stdvt, x_pos, y_pos, text_min, text_max): # median: where arrow is pointing, stdvt: standard deviation
         size = 200
@@ -74,41 +75,64 @@ class Application(tk.Frame):
         original_y = y_pos
         starts = [0,9,23,32,41,59,77,95]
         extents = [9,14,9,9,18,18,18,5]
-        colours = ['darkgreen','green','lightgreen','orange','lightpink','purple','lightblue','lightgrey']
-        for i in range(8):
+        colours = ['darkgreen','green','lightgreen','orange','lightblue','purple','lightgrey','grey']
+        for i in range(8): # value might change if graph is moved
             x = (((values[i] - 0) * (50 - 300)) / (100 - 0)) + 300
             y = (((values[i] - 0) * (50 - 300)) / (100 - 0)) + 300
             s = values[i]/100 * 500
             canvas.create_arc(x, y, s + x, s + y, start=starts[i]*3.6, extent=extents[i]*3.6,fill=colours[i], outline='')
-        
-        canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=0, extent=9*3.6, outline='gray',dash=(6, 5, 2, 4))
-        canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=9*3.6, extent=14*3.6, outline='gray',dash=(6, 5, 2, 4))
-        canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=23*3.6, extent=9*3.6, outline='gray',dash=(6, 5, 2, 4))
-        canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=32*3.6, extent=9*3.6, outline='gray',dash=(6, 5, 2, 4))
-        
-        canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=41*3.6, extent=18*3.6, outline='gray',dash=(6, 5, 2, 4))
-        canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=59*3.6, extent=18*3.6, outline='gray',dash=(6, 5, 2, 4))
-        canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=77*3.6, extent=18*3.6, outline='gray',dash=(6, 5, 2, 4))
-        canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=95*3.6, extent=5*3.6, outline='gray',dash=(6, 5, 2, 4))
 
         color = 'gray'
         for i in range(9):
             if i == 4:
                 color = 'red'
             else:
-                color = 'gray'
+                color = 'darkgrey'
             x_pos = x_pos + 25
             y_pos = y_pos + 25
             size = size - 50
-            canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=0, extent=9*3.6, outline=color,dash=(6, 5, 2, 4))
-            canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=9*3.6, extent=14*3.6, outline=color,dash=(6, 5, 2, 4))
-            canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=23*3.6, extent=9*3.6, outline=color,dash=(6, 5, 2, 4))
-            canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=32*3.6, extent=9*3.6, outline=color,dash=(6, 5, 2, 4))
+            canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=0, extent=9*3.6, outline=color,dash=(6, 5, 2, 4), width='1.25')
+            canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=9*3.6, extent=14*3.6, outline=color,dash=(6, 5, 2, 4), width='1.25')
+            canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=23*3.6, extent=9*3.6, outline=color,dash=(6, 5, 2, 4), width='1.25')
+            canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=32*3.6, extent=9*3.6, outline=color,dash=(6, 5, 2, 4), width='1.25')
             
-            canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=41*3.6, extent=18*3.6, outline=color,dash=(6, 5, 2, 4))
-            canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=59*3.6, extent=18*3.6, outline=color,dash=(6, 5, 2, 4))
-            canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=77*3.6, extent=18*3.6, outline=color,dash=(6, 5, 2, 4))
-            canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=95*3.6, extent=5*3.6, outline=color,dash=(6, 5, 2, 4))
+            canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=41*3.6, extent=18*3.6, outline=color,dash=(6, 5, 2, 4), width='1.25')
+            canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=59*3.6, extent=18*3.6, outline=color,dash=(6, 5, 2, 4), width='1.25')
+            canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=77*3.6, extent=18*3.6, outline=color,dash=(6, 5, 2, 4), width='1.25')
+            canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=95*3.6, extent=5*3.6, outline=color,dash=(6, 5, 2, 4), width='1.25')
+        
+        x_pos = original_x
+        y_pos = original_y
+        size = 500
+
+        canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=0, extent=9*3.6, outline='black', width='2')
+        canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=9*3.6, extent=14*3.6, outline='black', width='2')
+        canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=23*3.6, extent=9*3.6, outline='black', width='2')
+        canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=32*3.6, extent=9*3.6, outline='black', width='2')
+        
+        canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=41*3.6, extent=18*3.6, outline='black', width='2')
+        canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=59*3.6, extent=18*3.6, outline='black', width='2')
+        canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=77*3.6, extent=18*3.6, outline='black', width='2')
+        canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=95*3.6, extent=5*3.6, outline='black', width='2')
+ 
+    def menu_big_brain_graph(self, canvas):
+        canvas.create_rectangle(600, 50, 825, 75, fill='lightgrey')
+        canvas.create_text(712.5, 62.5,fill="Black",font="Verdana 11 bold", text='Overall Scoring')
+        canvas.create_rectangle(900, 50, 1125, 75, fill='lightgreen')
+        canvas.create_text(1012.5, 62.5,fill="Black",font="Verdana 11 bold", text='Presenteeism')
+        canvas.create_rectangle(600, 200, 825, 225, fill='orange')
+        canvas.create_text(712.5, 212.5,fill="Black",font="Verdana 11 bold", text='Engagement')
+        canvas.create_rectangle(900, 200, 1125, 225, fill='lightblue')
+        canvas.create_text(1012.5, 212.5,fill="Black",font="Verdana 11 bold", text='Absenteeism')
+        canvas.create_rectangle(600, 350, 825, 375, fill='purple')
+        canvas.create_text(712.5, 362.5,fill="White",font="Verdana 11 bold", text='Self-Assessment')
+        canvas.create_rectangle(900, 350, 1125, 375, fill='lightyellow')
+        canvas.create_text(1012.5, 362.5,fill="Black",font="Verdana 11 bold", text='Organizational Outputs')
+
+        canvas.create_rectangle(600, 500, 1125, 525, fill='lightgrey')
+    
+    def show_values_category(self, canvas, present, past, x_pos, y_pos):
+        
 
 app = Application(master=root)
 app.mainloop()
