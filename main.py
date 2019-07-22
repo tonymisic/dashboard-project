3436b2e502665e231d1a3eb2159256e96a42d960
Tony Misic <tony.misic98@gmail.com>
2019-07-22 14:44:58 -0400

    Added background 1 and 2

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ page1 = ttk.Frame(nb)
 page2 = ttk.Frame(nb)
 page3 = ttk.Frame(nb)
 page4 = ttk.Frame(nb)
-nb.add(page1, text='Dashboard 1')
+nb.add(page1, text='Fresh Pie')
 nb.add(page2, text='Environmental')
 nb.add(page3, text='Dashboard 3')
 nb.add(page4, text='Dashboard 4')
@@ -29,27 +29,49 @@ canvas4.pack(fill="both", expand=True)
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        self.winfo_toplevel().title("IBI Dashboard")
+        self.winfo_toplevel().title("Organization 1's Dashboard")
         self.master = master
         self.pack()
         #summer_temp_impact_score = self.get_excel_data("Input_for_Dashboard_for_MD.xlsx", 6, 2, 0)
         #summer_temp_impact_confidence = self.get_excel_data("Input_for_Dashboard_for_MD.xlsx", 6, 3, 0)
-        self.create_graph(canvas2, 0.13, 0.11, 100, 100, "-2 Far Too Dry", "+2 Far Too Humid")
-        self.create_graph(canvas2, -0.91, 0.11, 500, 100, "-2 Far Too Dry", "+2 Far Too Humid")
-        self.create_graph(canvas2, -0.11, 0.2, 900, 100, "-2 Far Too Dry", "+2 Far Too Humid")
-        self.create_graph(canvas2, -0.5, 1, 100, 400, "-2 Far Too Dry", "+2 Far Too Humid")
-        self.create_graph(canvas2, 1.11, 0.2, 500, 400, "-2 Far Too Dry", "+2 Far Too Humid")
-        self.create_graph(canvas2, -0.61, 0.1, 900, 400, "-2 Far Too Dry", "+2 Far Too Humid")
-        rgb = canvas1.winfo_rgb("plum2")
-        red, green, blue = rgb[0]/256, rgb[1]/256, rgb[2]/256
-        print("R: %d, G: %d, B: %d" %(red, green, blue))
+        # !!! needs to be connected to the excel sheet !!!
+        self.show_image(canvas2, 'background2.jpg', 600, 350)
+        self.create_graph(canvas2, 1, 0.54, 95, 266)
+        self.create_graph(canvas2, -0.91, 0.11, 364, 266)
+        self.create_graph(canvas2, -0.11, 0.2, 633, 266)
+        self.create_graph(canvas2, -0.5, 1, 95, 455)
+        self.create_graph(canvas2, 1.11, 0.2, 364, 455)
+        self.create_graph(canvas2, -0.61, 0.1, 633, 455)
+        self.create_graph(canvas2, -0.5, 1, 905, 81)
+        self.create_graph(canvas2, 1.11, 0.2, 905, 266)
+        self.create_graph(canvas2, -0.61, 0.1, 905, 455)
+
+        canvas2.create_text(70, 612, fill="Black", font="Verdana 13", text="Organization 1", anchor="w")
+        canvas2.create_text(70, 132, fill="Black", font="Verdana 40", text="54.76", anchor="sw") # add code for it to be 2 decimal points always
+        canvas2.create_text(230, 132, fill="Black", font="Verdana 20", text="/100", anchor="sw")
+
+        canvas2.create_text(195, 248, fill="Black", font="Verdana 13", text="1")
+        canvas2.create_text(464, 248, fill="Black", font="Verdana 13", text="-0.91")
+        canvas2.create_text(733, 248, fill="Black", font="Verdana 13", text="-0.11")
+
+        canvas2.create_text(195, 437, fill="Black", font="Verdana 13", text="-0.5")
+        canvas2.create_text(464, 437, fill="Black", font="Verdana 13", text="1.11")
+        canvas2.create_text(733, 437, fill="Black", font="Verdana 13", text="-0.61")
+
+        canvas2.create_text(1005, 63, fill="Black", font="Verdana 13", text="-0.5")
+        canvas2.create_text(1005, 248, fill="Black", font="Verdana 13", text="1.11")
+        canvas2.create_text(1005, 437, fill="Black", font="Verdana 13", text="-0.61")
+
+        canvas2.create_text(183, 632, fill="Black", font="Verdana 8", text="05/12/2018", anchor="w")
+        canvas2.create_text(183, 650, fill="Black", font="Verdana 8", text="16/07/2019", anchor="w")
+
         self.show_image(canvas1, 'background1.jpg', 600, 375)
         # [ organizational, environmental, behavioural, engagement, absenteeism, self, org-output, health]
         self.big_brain_graph(canvas1, [45,25,85,100,90,60,100,50], 50, 50)
         # [overall, presenteeism, engagement, absenteeism, self, org-output]
         self.menu_big_brain_graph(canvas1, [45,65,100,76,45,98], [45,65,100,90,45,100])  
-
-    def create_graph(self, canvas, median, stdvt, x_pos, y_pos, text_min, text_max): # median: where arrow is pointing, stdvt: standard deviation
+        
+    def create_graph(self, canvas, median, stdvt, x_pos, y_pos): # median: where arrow is pointing, stdvt: standard deviation
         size = 200
         text_offset = 110
         confidence = stdvt * 45
@@ -64,8 +86,6 @@ class Application(tk.Frame):
         canvas.create_arc(x_pos, y_pos, size + x_pos, size + y_pos, start=mid_point - confidence/2, extent=confidence,outline='lightgrey', fill='lightgrey') # standard deviation
         canvas.create_arc(x_pos, y_pos, size + x_pos, size + y_pos, start=mid_point, extent=0, fill='blue') # median
         canvas.create_arc(x_pos, y_pos, size + x_pos, size + y_pos, start=0, extent=180) # full graph
-        canvas.create_text(x_pos, y_pos + text_offset,fill="black",font="Times 10 bold", text=text_min)
-        canvas.create_text(x_pos + size, y_pos + text_offset,fill="black",font="Times 10 bold", text=text_max)
 
     def get_excel_data(self, filename, row, col, sheet_num):
         workbook = xlrd.open_workbook(filename)
@@ -120,41 +140,25 @@ class Application(tk.Frame):
         canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=59*3.6, extent=18*3.6, outline='darkgrey',dash=(6, 5, 2, 4), width='1.25')
         canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=77*3.6, extent=18*3.6, outline='darkgrey',dash=(6, 5, 2, 4), width='1.25')
         canvas.create_arc(x_pos,y_pos,size + x_pos,size + y_pos, start=95*3.6, extent=5*3.6, outline='darkgrey',dash=(6, 5, 2, 4), width='1.25')
-        canvas.create_line(300, 300, 550, 300, fill='black', width='2')
-        canvas.create_line(300, 300, 511.11, 166.03, fill='black', width='2')
-        canvas.create_line(300, 300, 331.34, 51.95, fill='black', width='2')
-        canvas.create_line(300, 300, 193.53, 73.74, fill='black', width='2')
+        canvas.create_line(300, 300, 550, 300, fill='black', width='1.5')
+        canvas.create_line(300, 300, 511.11, 166.03, fill='black', width='1.5')
+        canvas.create_line(300, 300, 331.34, 51.95, fill='black', width='1.5')
+        canvas.create_line(300, 300, 193.53, 73.74, fill='black', width='2.5')
 
-        canvas.create_line(300, 300, 89.95, 164.37, fill='black', width='2')
-        canvas.create_line(300, 300, 88.89, 433.97, fill='black', width='2')
-        canvas.create_line(300, 300, 331.34, 548.05, fill='black', width='2')
-        canvas.create_line(300, 300, 537.83, 377.28, fill='black', width='2')
+        canvas.create_line(300, 300, 89.95, 164.37, fill='black', width='2.5')
+        canvas.create_line(300, 300, 88.89, 433.97, fill='black', width='2.5')
+        canvas.create_line(300, 300, 331.34, 548.05, fill='black', width='2.5')
+        canvas.create_line(300, 300, 537.83, 377.28, fill='black', width='2.5')
  
     def menu_big_brain_graph(self, canvas, values_past, values_present):
-        canvas.create_line(75,575,150,575, dash=(6, 5, 2, 4), width='1.25')
-        canvas.create_line(250,575,325,575, dash=(6, 5, 2, 4), width='1.25', fill='red')
-        #canvas.create_text(200, 575,fill="Black",font="Verdana 8", text='10% Interval')
-        #canvas.create_text(400, 575,fill="Black",font="Verdana 8", text='50% or Neutral Impact')
-        #canvas.create_rectangle(600, 50, 825, 75, fill='lightgrey')
-        #canvas.create_text(712.5, 62.5,fill="Black",font="Verdana 11 bold", text='Overall Scoring')
+        canvas.create_line(110,585,185,585, dash=(6, 5, 2, 4), width='1.25')
+        canvas.create_line(310,585,385,585, dash=(6, 5, 2, 4), width='1.25', fill='red')
         self.show_values_overall(canvas, values_present[0], values_past[0], 600, 75)
-        #canvas.create_rectangle(900, 50, 1125, 75, fill='lightgreen')
-        #canvas.create_text(1012.5, 62.5,fill="Black",font="Verdana 11 bold", text='Presenteeism')
         self.show_values_category(canvas, values_present[1], values_past[1], 900, 75)
-        #canvas.create_rectangle(600, 200, 825, 225, fill='tan1')
-        #canvas.create_text(712.5, 212.5,fill="Black",font="Verdana 11 bold", text='Engagement')
         self.show_values_category(canvas, values_present[2], values_past[2], 600, 225)
-        #canvas.create_rectangle(900, 200, 1125, 225, fill='lightblue')
-        #canvas.create_text(1012.5, 212.5,fill="Black",font="Verdana 11 bold", text='Absenteeism')
         self.show_values_category(canvas, values_present[3], values_past[3], 900, 225)
-        #canvas.create_rectangle(600, 350, 825, 375, fill='MistyRose2')
-        #canvas.create_text(712.5, 362.5,fill="White",font="Verdana 11 bold", text='Self-Assessment')
         self.show_values_category(canvas, values_present[4], values_past[4], 600, 375)
-        #canvas.create_rectangle(900, 350, 1125, 375, fill='lightyellow')
-        #canvas.create_text(1012.5, 362.5,fill="Black",font="Verdana 11 bold", text='Organizational Outputs')
         self.show_values_category(canvas, values_present[5], values_past[5], 900, 375)
-
-        #canvas.create_rectangle(600, 500, 1125, 525, fill='lightgrey')
     
     def show_values_category(self, canvas, present, past, x_pos, y_pos):
         color_circle = 'white'
@@ -198,6 +202,11 @@ class Application(tk.Frame):
         img = Image.open(filename)
         canvas.image = ImageTk.PhotoImage(img)
         canvas.create_image(x, y, image=canvas.image)
+
+    def find_rgb(self, colour):
+        rgb = canvas1.winfo_rgb(colour)
+        red, green, blue = rgb[0]/256, rgb[1]/256, rgb[2]/256
+        print("R: %d, G: %d, B: %d" %(red, green, blue))
 
 app = Application(master=root)
 app.mainloop()
